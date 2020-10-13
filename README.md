fcbda1a5c275598048ff6f0b585072bef42b4f24
mariaconejo <mary03081@gmail.com>
2020-10-12 20:18:06 -0600

    Modifico el html y js para cambiar el valor del src

--- README.md
+++ README.md
@@ -2,14 +2,11 @@ Autores María José Conejo Leiva, Jeampaul Salazar Castillo.
 
 Galería de imágenes
 
-● Inicialmente se muestra la primera imagen en grande y miniaturas de todas las imágenes de la galería:
-
-Todas la imágenes estarán presentes en el HTML, sin embargo con el JS dejaremos visible únicamente la primera imagen de la previsualización y resaltada con un estilo, además en el gallery-content se vera dicha imagen en macro. Esto se logró utilizando classList add y classList remove, con clases en el css que manejaban la visibilidad, además con un for se recorrió el gallery-content, para manejar dicha visibilidad y ocultar las otras imágenes en grande, con img_content[0] y imgs[0], para visualizar desde un principio la primera imagen en grande y la miniatura seleccionada.
-
 ● Al hacer click en una miniatura, la imagen grande cambia:
 
-Se crea una función mediante la cual se oculten las imágenes que no queremos ver interviniendo en el HTML añadiendo y quitando características, en otras palabras que solo se visualice la imagen a la que le damos click (evento).Esto se logró utilizando href en el gallery-prev, donde están la lista ul con las miniaturas y id en gallery-content donde están todas las imágenes en grande, las enlazamos colocando la misma nomenclatura en href y id, al seleccionar la miniatura nos redirigía a la grande.
+Todas la imágenes estarán contenidas en una carpeta llamada img, mediante el JS modificaremos la ubicacion de las imagenes con una funcion que posea un ciclo concatenando el src de la miniatura gracias al evento click, con el contenedor de la visualización grande utilizando ".getAttribute" y sobreescribiendo el valor de la imagen que esta por default. 
+
 
 ● Se destaca la imagen actual en la lista de miniaturas:
 
-Mediante la función antes mencionada se le muestra o no al usuario un estilo que permita que la imagen seleccionada sea el punto focal en la previsualización. Esto se logró utilizando la clase img-active del css para destacar las miniaturas, cuando se seleccionen usándolo en conjunto con classList para agregar los estilos con js.
+Mediante la función antes mencionada con un nuevo ciclo se le muestra o no al usuario un estilo removiendo y colocando clases que permitan que a la imagen que se le da click, resalte entre las demás, en la previsualización. Esto se logró utilizando la clase img-active del css con filter:blur para destacar las miniaturas, cuando se seleccionen.
--- js/scritps.js
+++ js/scritps.js
@@ -1,42 +1,27 @@
-//Variables que obtinenen los enlaces de miniaturas y divs contenedores con las imagenes grandes
-let imgs = document.querySelectorAll('.gallery-list-img');
-let img_content = document.querySelectorAll('.gallery-content-img');
+//Variables que obtinenen los enlaces de miniaturas y el div contenedor con la imagen en grande
+const links = document.querySelectorAll('.miniature');
+const bigImage = document.getElementById('bigImage');
 
+//Funcion principal de la Galeria
+for (let i = 0; i < links.length; i++){ 
+    links[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        //De esta forma se abstrae la ubicacion de una imagen y se modifica en el div grande (src)
+        let url = event.currentTarget.getAttribute('src');
+        bigImage.src = `${url}`;
 
-
-//for que recorre el img-content y  oculta las imagenes en grande y solo deja visible una
-for(let i = 0 ; i < img_content.length; i++){
-    img_content[i].classList.add('img-content-hidden');
-}
-//CODIGO Inicialmente se muestra la primera imagen en grande y miniaturas de todas las imágenes de la galería
-
-// muestra la primera imagen en grande y la miniatura seleccionada
-img_content[0].classList.remove('img-content-hidden');
-imgs[0].classList.add('img-active');
-//-------------------------------------------------------------------------------------------------------------------
-
-//CODIGO Al hacer click en una miniatura, la imagen grande cambia
-//CODIGO Se destaca la imagen actual en la lista de miniaturas
-
-// funcion con un for   que recorre todas las minaturas, al darle clik (evento) 
-//pueda vizualizarse solo una imagen en grande que coincide con el id  de las miniaturas
-//por eso lo hicimos con href
-for (let i = 0; i < imgs.length; i++){
-    imgs[i].addEventListener('click', (event) => {
-        event.preventDefault(); // elimina la funcionalidad default de los href
-        const id = event.currentTarget.getAttribute('href').substring(1); // elimina el # de los id
-        for(let i = 0 ; i < img_content.length; i++){
-            img_content[i].classList.add('img-content-hidden');
-            if (img_content[i].getAttribute('id') == id){   // muestra solo la imagen grande que coincida con el id de la miniatura
-                img_content[i].classList.remove('img-content-hidden');
+        //De esta forma se aplica un estilo para cada miniatura en donde se da click
+        for(let i = 0 ; i < links.length; i++){
+            links[i].classList.add('img-content-hidden');
+            if (links[i].getAttribute('src') == url){   
+                links[i].classList.remove('img-content-hidden');
             }
         }
-        for(let i = 0; i < imgs.length; i++){
-            imgs[i].classList.remove('img-active');
+        for(let i = 0; i < bigImage.length; i++){
+            bigImage[i].classList.remove('img-active');
         }
         event.currentTarget.classList.add('img-active');
     });
+    
 }
-
-
 
